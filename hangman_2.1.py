e2acdf7606be6b730e3af05df63ccedaa87f5a55
bartoszmaleta <bartosz.maleta@gmail.com>
2019-10-15 10:39:27 +0200

    Working while, added list of cc

--- hangman_2.1.py
+++ hangman_2.1.py
@@ -8,6 +8,7 @@ list_of_password_chars = []
 list_of_dashes = []
 list_of_indexes = []
 list_of_not_in_word_letters = []
+list_of_random_line = []
 chances = 10
 
 print()
@@ -20,30 +21,49 @@ print(list_of_password_chars)
 
 for i in range(len(password)):
     list_of_dashes.append(str('-'))
+print('asdasds', 1)
 
 print(list_of_dashes)
 print()
+with open('countries-and-capitals.txt') as hangman_password_list:
+    for i, line in enumerate(hangman_password_list):
+        print(f'{format(i+1)} {line.strip()}')
+    hangman_password_list.close()
 
-guess_letter = input('What is your letter? ')
+print('++++++++++++++')
+with open('countries-and-capitals.txt') as printing_random_line:
+    random_line = printing_random_line.readlines()
+    print(random.choice(random_line))
+    hangman_password_list.close()
+    list_of_random_line.append(random_line[i], end=" ")
+
+print(list_of_random_line)
 
 # here insert "while" loop
+while list_of_password_chars != list_of_dashes and chances > 0:
+    print('*********************************************', 'You have: ', chances, 'left')
+    guess_letter = input('What is your letter? ')
+    if guess_letter in list_of_password_chars:
+        print('yes it is')
+        for i, j in enumerate(list_of_password_chars):
+            if j == guess_letter:
+                print(i)
+                list_of_dashes[i] = guess_letter
+                print(list_of_dashes)
+              
+    else:
+        list_of_not_in_word_letters.append(guess_letter)
+        chances -= 1
+        print('No, it is not')
+        print(list_of_dashes)
+        print('The letters you have already used: ')
+        print(list_of_not_in_word_letters)
+        for x, value in enumerate(list_of_not_in_word_letters):
+            print(x)
 
-if guess_letter in list_of_password_chars:
-    print('yes it is')
-    for i, j in enumerate(list_of_password_chars):
-        if j == guess_letter:
-            print(i)
-            list_of_dashes[i] = guess_letter
 
-    print()
-else:
-    list_of_not_in_word_letters.append(guess_letter)
-    
-    print('No, it is not')
-    print()
-
-print(list_of_dashes)
-print('The letters you have already used: ')
-print(list_of_not_in_word_letters)
-for x, value in enumerate(list_of_not_in_word_letters):
-    print(x)
+# print(list_of_dashes)
+# print('The letters you have already used: ')
+# print(list_of_not_in_word_letters)
+# for x, value in enumerate(list_of_not_in_word_letters):
+#     print(x)
