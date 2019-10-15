04675c3fc264cf5c7baf1c65e49603e093b0b731
bartoszmaleta <bartosz.maleta@gmail.com>
2019-10-15 10:38:40 +0200

    Working while&added list of cc

--- hangman-bartosz_try.py
+++ hangman-bartosz_try.py
@@ -4,29 +4,43 @@ word = 'piwop'
 guesses = ''
 choices = 10
 
-while choices > 0:
-    wrong_answears = 0
-    for chars in word:
-        if chars in guesses:
-            print(chars, end='')
-        else:
-            print('-', end='')
-            wrong_answears += 1
-    if wrong_answears == 0:
-        print('You won')
-        exit()
-    
-    print()
-    print('*************************************')
+while True:
+    while choices > 0:
+        wrong_answears = 0
+        for chars in word:
+            if chars in guesses:
+                print(chars, end='')
+            else:
+                print('-', end='')
+                wrong_answears += 1
+        if wrong_answears == 0:
+            print('You won')
+            print('*******************************')
+            print('Welcome again')
+            print('Lets play again')
 
-    guess = input('What is your character? ')
-    guesses += guess
-    
-    if guess not in word:
-        choices -= 1
-        print('Wrong')
-        print('You have', choices, 'more guesses')
+        print()
         print('*************************************')
-        
+
+        guess = input('What is your character? ')
+        guesses += guess
+
+        if guess not in word:
+            choices -= 1
+            print('Wrong')
+            print('You have', choices, 'more guesses')
+            print('*************************************')
+
         if choices == 0:
             print('You loose')
+    
+    while True:
+        answear = input('Run again? (y/n): ')
+        if answear in ('y', 'n'):
+            break
+        print('Invalid input. ')
+    if answear == 'y':
+        continue
+    else:
+        print('Goodbye')
+        break
