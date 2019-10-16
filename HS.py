2e9e7acb22e454c072deda9f7310e0c101fc9059
bartoszmaleta <bartosz.maleta@gmail.com>
2019-10-16 20:20:38 +0200

    HS.py and .txt added to main folder

---
+++ HS.py
@@ -0,0 +1,45 @@
+import operator
+
+HS_dict = {}
+
+with open("HS.txt") as f:
+    for line in f:
+        (key, val) = line.split("|")
+        HS_dict[(key)] = int(val.rstrip('\n'))
+
+print(HS_dict)
+
+sorted_HS = sorted(HS_dict.items(), key=operator.itemgetter(-1))
+
+print(sorted_HS)
+    
+x = 0
+y = 0
+spaces = ' '
+
+
+# spaces_20 = spaces * 20
+spaces_40 = spaces * 40
+print(spaces_40)
+
+# length_of_key = int(len(key))
+# length_of_val = int(len(val))
+# number_of_spaces = 50 - length_of_key - length_of_val
+# print(number_of_spaces)
+
+# trailing_spaces = int(number_of_spaces) * spaces
+# print(trailing_spaces)
+
+print(sorted_HS)
+print('                    ', "Hall of Fame")
+print('Name', '                                        ', 'Score')
+
+for key, val in sorted_HS:
+    length_of_key = len(key)
+    #  print(length_of_key)
+    val_str = str(val)
+    length_of_val = len(val_str)
+    number_of_spaces = 50 - length_of_key - length_of_val
+    trailing_spaces = int(number_of_spaces) * spaces
+    print(val, trailing_spaces, key)
+    # print(val, spaces_40, key)
